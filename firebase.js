96a5c6dc2586b82bd17bc7d39e0759420dc95eca
anakin-vader20 <89256588+anakin-vader20@users.noreply.github.com>
2024-08-02 15:08:26 +0800

    Possible Error access for firebase

--- firebase.js
+++ firebase.js
@@ -1,22 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAzwe3fQSuBhXF5X7QDrxWDsgn9iZuiK6s",
-  authDomain: "pantry-app-cbbd1.firebaseapp.com",
-  projectId: "pantry-app-cbbd1",
-  storageBucket: "pantry-app-cbbd1.appspot.com",
-  messagingSenderId: "1010343946896",
-  appId: "1:1010343946896:web:9d681082d253446f56c615"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
+
+console.log(firebaseConfig)
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
-export {
-    app, firestore
-}
+export { firestore }
