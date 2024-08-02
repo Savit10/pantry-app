4ca6751ef58508a58962c8b0d38a4ab1097a197a
anakin-vader20 <89256588+anakin-vader20@users.noreply.github.com>
2024-08-02 13:20:24 +0800

    Attempt#3 fixing Vercel Firebase error

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import {firebaseconfig} from "@/firebase.js";
+import {firebaseconfig} from "Pantry-App/pantry-app/firebase.js";
 
 const inter = Inter({ subsets: ["latin"] });
 
