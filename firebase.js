05edfa3979f7f3fc04403a9b1dc50a03235ae5c8
anakin-vader20 <89256588+anakin-vader20@users.noreply.github.com>
2024-08-02 17:49:10 +0800

    Fixing public secret access

--- firebase.js
+++ firebase.js
@@ -1,11 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+console.log('Storage Bucket:', process.env.REACT_APP_FIREBASE_STORAGE_BUCKET);
+console.log('Messaging Sender ID:', process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID);
+console.log('App ID:', process.env.REACT_APP_FIREBASE_APP_ID);
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  projectId: "pantry-app-cbbd1",
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID
